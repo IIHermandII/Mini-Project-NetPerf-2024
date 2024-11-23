701d0e2f6f65bcf568dab60bb233c1cd8f606ac3
Hermand <hermand8320@gmail.com>
2024-11-23 13:35:00 +0100

    Words Caraktors function in shannon works

--- Shannon Encoder/Shannon.py
+++ Shannon Encoder/Shannon.py
@@ -1,5 +1,34 @@
+
+
+def Get_MobyDick_Data(WordsOrCaraktors):
+    MobyDickDataArray=[]
+    # We wish the data array to be in Caraktors
+    if WordsOrCaraktors == "Caraktors":
+        print("C")
+        try:
+            with open("Novel\\Moby Dick.txt", "r", encoding="utf-8") as f:
+                for x in f:
+                    x = list(x)   # You can still print the lines if you need to debug
+                    MobyDickDataArray.extend(x)
+        except UnicodeDecodeError as e:
+            print(f"Error reading the file: {e}")
+    # We wish the data array to be in Words
+    else:
+        print("Words chousen")
+        try:
+            with open("Novel\\Moby Dick.txt", "r", encoding="utf-8") as f:
+                for x in f:
+                    x = x.split()   # You can still print the lines if you need to debug
+                    MobyDickDataArray.extend(x)
+        except UnicodeDecodeError as e:
+            print(f"Error reading the file: {e}")
+
+    return MobyDickDataArray
+
 def main():
     print("Shannon")
+    Data = Get_MobyDick_Data("Caraktors")
+    print(Data)
 
 if __name__ == "__main__":
     main()
